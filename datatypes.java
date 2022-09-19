ae812cecc886dbd0aa05b339d8396adde6ca3afc
Anıl Numan Şahin <108685051+anilnuman@users.noreply.github.com>
2022-09-19 20:40:22 +0300

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        double sayi = 12.5;
+        sayi = -129;
+
+        char karekter = 'A';
+
+        boolean dogruMu = false;
+
+    }
+}
